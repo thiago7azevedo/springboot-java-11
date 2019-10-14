827dbb5163e0a5f092b90a207f7ea4c7a249524f
Thiago Azevedo <thiago7azevedo@gmail.com>
2019-10-14 19:32:38 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/dianyzathelli/course/config/TestConfig.java
+++ src/main/java/com/dianyzathelli/course/config/TestConfig.java
@@ -50,6 +50,16 @@ public class TestConfig implements CommandLineRunner { // implements a interface
 		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));// salva no BD
 		productrepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 		
+//		Assossiação entre os objetos para a tabela relacional do H2		
+		p1.getCategories().add(cat2);
+		p2.getCategories().add(cat1);
+		p2.getCategories().add(cat3);
+		p3.getCategories().add(cat3);
+		p4.getCategories().add(cat3);
+		p5.getCategories().add(cat2);
+		
+		productrepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+		
 		//instanciando objetos da classe User com 5 atributos
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
--- src/main/java/com/dianyzathelli/course/entities/Category.java
+++ src/main/java/com/dianyzathelli/course/entities/Category.java
@@ -8,8 +8,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
-import javax.persistence.Transient;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name="tb_category")
@@ -21,7 +23,8 @@ public class Category implements Serializable {
 	private Long id;
 	private String name;
 	
-	@Transient
+	@JsonIgnore
+	@ManyToMany(mappedBy = "categories")
 	private Set<Product> products = new HashSet<>();
 //  instancia para garantir que não começe nula, mas sim vazia
 //  utilizando uma coleção Set, que representa um conjunto. Garante que não vai ter produto com mais de uma ocorrencia da mesma categoria
